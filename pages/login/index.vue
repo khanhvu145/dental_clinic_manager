b4bfad292876fbd55ec503119c308ac6d0f0c243
khanhvu145 <khanhvn@twin.vn>
2022-09-02 14:42:40 +0700

    update giao dien

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -4,39 +4,29 @@
             <div class="logo">
                 <img src="/images/logoclinic.png" alt="">
             </div>
+            <div class="mt-4" style="opacity:0.6; font-size:14px; text-align:center;">
+                Nhập tài khoản & mật khẩu để đăng nhập
+            </div>
             <div class="input-box">
-                <div class="username">
-                    <svg class="svg-icon" viewBox="0 0 20 20">
-                        <path d="M12.075,10.812c1.358-0.853,2.242-2.507,2.242-4.037c0-2.181-1.795-4.618-4.198-4.618S5.921,4.594,5.921,6.775c0,1.53,0.884,3.185,2.242,4.037c-3.222,0.865-5.6,3.807-5.6,7.298c0,0.23,0.189,0.42,0.42,0.42h14.273c0.23,0,0.42-0.189,0.42-0.42C17.676,14.619,15.297,11.677,12.075,10.812 M6.761,6.775c0-2.162,1.773-3.778,3.358-3.778s3.359,1.616,3.359,3.778c0,2.162-1.774,3.778-3.359,3.778S6.761,8.937,6.761,6.775 M3.415,17.69c0.218-3.51,3.142-6.297,6.704-6.297c3.562,0,6.486,2.787,6.705,6.297H3.415z"></path>
-                    </svg>
-                    <input
-                        class="user-input"
-                        type="text"
-                        id="username"
-                        v-model="login.username"
-                        required=""
-                        placeholder="Nhập tài khoản"
-                    />
+                <div class="usename_box">
+                    <el-input placeholder="Tài khoản" v-model="login.username">
+                        <i slot="prefix" class="el-input__icon el-icon-user-solid"></i>
+                    </el-input>
                 </div>
-                <div class="password">
-                    <svg class="svg-icon" viewBox="0 0 20 20">
-                        <path d="M17.308,7.564h-1.993c0-2.929-2.385-5.314-5.314-5.314S4.686,4.635,4.686,7.564H2.693c-0.244,0-0.443,0.2-0.443,0.443v9.3c0,0.243,0.199,0.442,0.443,0.442h14.615c0.243,0,0.442-0.199,0.442-0.442v-9.3C17.75,7.764,17.551,7.564,17.308,7.564 M10,3.136c2.442,0,4.43,1.986,4.43,4.428H5.571C5.571,5.122,7.558,3.136,10,3.136 M16.865,16.864H3.136V8.45h13.729V16.864z M10,10.664c-0.854,0-1.55,0.696-1.55,1.551c0,0.699,0.467,1.292,1.107,1.485v0.95c0,0.243,0.2,0.442,0.443,0.442s0.443-0.199,0.443-0.442V13.7c0.64-0.193,1.106-0.786,1.106-1.485C11.55,11.36,10.854,10.664,10,10.664 M10,12.878c-0.366,0-0.664-0.298-0.664-0.663c0-0.366,0.298-0.665,0.664-0.665c0.365,0,0.664,0.299,0.664,0.665C10.664,12.58,10.365,12.878,10,12.878"></path>
-                    </svg>
-                    <input
-                        type="password"
-                        id="password"
-                        v-model="login.password"
-                        class="pass-input"
-                        required=""
-                        placeholder="Nhập mật khẩu"
-                    />
+                <div class="password_box mt-3">
+                    <el-input placeholder="Mật khẩu" v-model="login.password" show-password>
+                        <i slot="prefix" class='el-input__icon bx bxs-lock' ></i>
+                    </el-input>
                 </div>
             </div>
-            <div class="checkbox">
-                <input type="checkbox" name="" id="remember">
-                <label for="remember">Nhớ mật khẩu</label>
+            <button type="submit" class="login-button mt-4">Đăng nhập</button>
+            <div class="text-center mt-4" style="opacity:0.8; font-size:14px;">
+                <nuxt-link to="/forgotpassword" class="sidebar-nav-link">
+                    <a href="javascript:void(0)">
+                        <span>Quên mật khẩu?</span>
+                    </a>
+                </nuxt-link>
             </div>
-            <button type="submit" class="login-button">Login</button>
         </form>
     </div>
 </template>
@@ -98,17 +88,17 @@ export default {
     align-items: center;
     justify-content: center;
     color: #555;
-    background-color: #ecf0f3;
+    background-color: #f0f2f5;
 }
 
 .login-form{
     width: 375px;
-    height: 580px;
+    height: 520px;
     padding: 60px 35px 35px 35px;
-    border-radius: 40px;
-    background-color: #ecf0f3;
-    box-shadow: 13px 13px 20px #cbced1,
-    -13px -13px 20px #fff;
+    border-radius: 24px;
+    background-color: #fff;
+    box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
+    border: none;
 }
 
 .logo {
@@ -138,7 +128,7 @@ export default {
 
 .input-box{
     width: 100%;
-    padding: 45px 5px 1px 5px;
+    padding: 30px 5px 1px 5px;
 }
 
 .input-box input{
@@ -168,30 +158,12 @@ export default {
     height: 22px;
     margin: 0 10px -3px 25px;
 }
-
-.checkbox {
-    text-align: left;
-    padding: 10px;
-    margin-bottom: 10px;
-}
-
-.checkbox input{
-    cursor: pointer;
-    top: 10px;
-}
-
-.checkbox label{
-    font-size: 14px;
-    font-weight: 300;
-    cursor: pointer;
-}
-
 .login-button {
     outline: none;
     border: none;
     cursor: pointer;
     width: 100%;
-    height: 60px;
+    height: 45px;
     border-radius: 30px;
     font-size: 20px;
     font-weight: 700;
@@ -203,8 +175,7 @@ export default {
     -3px -3px 8px #fff;
     transition: all .3s;
 }
-
 .login-button:hover {
-    background-color: rgb(75, 231, 88);
+    background-color: rgb(33, 168, 44);
 }
 </style>
